6e2abab22aa721467fad1d640aa8641a23391078
Rahafhosari <rahaf.hussary@gmail.com>
2020-12-22 18:55:07 +0200

    Changed

--- Job_app/models.py
+++ Job_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import re
+import bcrypt
 
-# Create your models here.
+# def user_login(login_info):
+#     user_exist = User.objects.filter(email=login_info['email'])
+#     if len(user_exist):
+#         password= login_info['password']
+#         if bcrypt.checkpw(password.encode(), user_exist[0].password.encode()):
+#             return user_exist[0]
+#     return False
--- Job_app/views.py
+++ Job_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . import models
 # Create your views here.
+
+def registration(request):
+    return render(request,"registration.html")
+
+def profile(request):
+    return render(request,"profile.html")
+
