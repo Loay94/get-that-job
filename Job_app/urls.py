4dc7c5a6848bad196be07a9333fd20773f920ab3
Loay94 <jodhloay@gmail.com>
2020-12-22 22:49:35 +0200

    add new html page (home_in)

--- Job_app/urls.py
+++ Job_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('in',views.home_in),
 ]
--- Job_app/views.py
+++ Job_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect, HttpResponse
 
-def home(request):
+# to view the main page without sign in:
+def home(request): 
     return render(request, "home_page.html")
+# to view the main page after sign in :
+def home_in(request):
+    return render(request, "home_in.html")
