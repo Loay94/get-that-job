f8207ceae82140c989457bb1a35f1eb0f281322f
TalaQawasmi <qawasmitala@gmail.com>
2020-12-23 11:20:49 +0200

    added booking and partners files

--- Job_app/views.py
+++ Job_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+def booking(request):
+    return render(request,'booking.html')
+    
+def partner(request):
+    return render(request,'partners.html')
 # Create your views here.
